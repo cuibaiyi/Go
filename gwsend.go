4ff37646604a12c1236ffc25d12f9de7fddb550f
carlos <619876714@qq.com>
2021-03-09 11:26:59 +0800

    Update gwsend.go

--- gwsend.go
+++ gwsend.go
@@ -14,7 +14,6 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-// 接收调用网关返回的数据获取token
 type Token struct {
 	Access_token string `json:"access_token"`
 	Token_type   string `json:"token_type"`
@@ -29,7 +28,6 @@ type R1 struct {
 	ErrReason   string `json:"errReason"`
 }
 
-// 接收统一通知返回的数据
 type Send struct {
 	Result     R1     `json:"result"`
 	RespDesc   string `json:"respDesc"`
@@ -41,45 +39,16 @@ var (
 	ConfigPath string
 	SendData   string
 	xml        = `
-	<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sms="http://www.sms.upbms.hp.com">
+	<soapenv:Envelope xmlns:soapenv="http://xxx" xmlns:sms="http://xxx">
     <soapenv:Header/>
-    <soapenv:Body>
-      <sms:smsService>
-         <!--Optional:-->
-         <target>5400</target>
-         <!--Optional:-->
-         <action>00</action>
-         <!--Optional:-->
-         <xmldata><![CDATA[%s]]></xmldata>
-         <!--Optional:-->
-         <brief>%s</brief>
-      </sms:smsService>
-    </soapenv:Body>
-    </soapenv:Envelope>
 	`
 	gwapi    = "http://xxx"
 	contType = "application/json"
 	api      = "http://xxx?"
 	d        = `{
-	"otherId": "1996092910",
-	"smsTitle": "",
-	"smsContent":"",
-	"ynDefaultNum":"N",
-	"sendNum":"",
-	"ynTimeSend":"N",
-	"timeNum":"20191202155000",
-	"ynUseHeibai":"N",
-	"heiOrBai":"B",
 	"receiveNums":"%s",
-	"ynTemplate":"Y",
 	"templateNum":"%s",
 	"dynaVariablle":%s,
-	"ynReply":"N",
-	"replyUrl":null,
-	"pageSize":10,
-	"currentPage":1,
-	"provinceCode":"HQ",
-	"appCode":"iHR",
 	"inputExt":"JSON"
 	}`
 )
@@ -103,12 +72,12 @@ func main() {
 	d1 := fmt.Sprintf(d, number, templateid, SendData)
 	token := GetGwToken(gwapi)
   // 拼接url
-	c := url.Values{"method": {"M_ABILITY_UNINOTI_107000001"}, "format": {"json"}, "appId": {"10041"}, "version": {"V1.0"}, "accessToken": {token}, "sign": {"3D993BD22F44E6C1A638D832DB2BFB6C8BA4116BEE1B3561B4B198DB7BB6B1A0"}, "timestamp": {"20190313162400"},
+	c := url.Values{"method": {"xxx"}, "format": {"json"}, "appId": {"x"}, "version": {"V1.0"}, "accessToken": {token}, "sign": {"xxx"}, "timestamp": {"xxx"},
 		"content": {d1}}
 	tzapi := fmt.Sprint(api, c.Encode())
 	fmt.Println(tzapi)
 	code := GetSendCode(tzapi)
-	fmt.Printf("请求统一通知返回的状态码%s\n", code)
+	fmt.Printf("请求接口返回的状态码%s\n", code)
 	if code != "00000" {
 		directSend(number, mesapi)
 	}
@@ -139,7 +108,7 @@ func md5V(str string) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-// 调用统一通知接口
+// 调用接口
 func GetSendCode(tzapi string) string {
 	resp, err := http.Post(tzapi, contType, nil)
 	if err != nil {
@@ -157,7 +126,7 @@ func GetSendCode(tzapi string) string {
 	return objsend.RespCode
 }
 
-// 获取网关token
+// 获取gw token
 func GetGwToken(gwapi string) string {
 	r, err := http.Get(gwapi)
 	if err != nil {
